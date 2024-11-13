7d5df28010ba6c9496d43b15681b7a5124077310
1m0rr1v3l <lsxyz2017@gmail.com>
2024-11-13 19:09:35 -0300

    Commit 2 - Replace Temp With Query

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function gerarFaturaStr (fatura, pecas) {
   const formato = new Intl.NumberFormat("pt-BR",
                         { style: "currency", currency: "BRL",
                           minimumFractionDigits: 2 }).format;
+  
+  // Função query que substitui a variável local "peca"
+  function getPeca(apresentacao) {
+      return pecas[apresentacao.id];
+  }
   //funcao extraido
   function calcularTotalApresentacao(apre, peca) {
     let total = 0;
@@ -32,16 +37,16 @@ function gerarFaturaStr (fatura, pecas) {
   }
 
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
+    const peca = getPeca(apre);
     let total = calcularTotalApresentacao(apre, peca);
 
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
